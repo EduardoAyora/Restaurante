0c9291e7e126c2757910b265c98096bf05f6a4b1
DELL <jramirezc9@est.ups.edu.ec>
2019-05-22 10:45:03 -0500

    Adición de ventanas para cliente y modificación de vista gerente y vista caja

--- src/ec/edu/ups/vista/VistaGerente.java
+++ src/ec/edu/ups/vista/VistaGerente.java
@@ -34,6 +34,12 @@ public class VistaGerente extends javax.swing.JInternalFrame {
         opUpdateMe = new javax.swing.JMenuItem();
         opDeleteMe = new javax.swing.JMenuItem();
         opListMe = new javax.swing.JMenuItem();
+        menMesa = new javax.swing.JMenu();
+        opCrearMesa = new javax.swing.JMenuItem();
+        opReadMesa = new javax.swing.JMenuItem();
+        opUpdateMesa = new javax.swing.JMenuItem();
+        opDeleteMesa = new javax.swing.JMenuItem();
+        opListMesa = new javax.swing.JMenuItem();
         menProduct = new javax.swing.JMenu();
         opCrearP = new javax.swing.JMenuItem();
         opReadP = new javax.swing.JMenuItem();
@@ -75,6 +81,30 @@ public class VistaGerente extends javax.swing.JInternalFrame {
         menMesero.add(opListMe);
 
         jMenuBar1.add(menMesero);
+
+        menMesa.setText("Mesa");
+
+        opCrearMesa.setText("Nuevo");
+        menMesa.add(opCrearMesa);
+
+        opReadMesa.setText("Buscar");
+        opReadMesa.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                opReadMesaActionPerformed(evt);
+            }
+        });
+        menMesa.add(opReadMesa);
+
+        opUpdateMesa.setText("Editar");
+        menMesa.add(opUpdateMesa);
+
+        opDeleteMesa.setText("Eliminar");
+        menMesa.add(opDeleteMesa);
+
+        opListMesa.setText("Lista");
+        menMesa.add(opListMesa);
+
+        jMenuBar1.add(menMesa);
 
         menProduct.setText("Producto");
 
@@ -152,26 +182,36 @@ public class VistaGerente extends javax.swing.JInternalFrame {
         // TODO add your handling code here:
     }//GEN-LAST:event_opReadCaActionPerformed
 
+    private void opReadMesaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_opReadMesaActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_opReadMesaActionPerformed
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JMenuBar jMenuBar1;
     private javax.swing.JMenu menCategoria;
+    private javax.swing.JMenu menMesa;
     private javax.swing.JMenu menMesero;
     private javax.swing.JMenu menProduct;
     private javax.swing.JMenuItem opCrearMe;
+    private javax.swing.JMenuItem opCrearMesa;
     private javax.swing.JMenuItem opCrearP;
     private javax.swing.JMenuItem opCreateCa;
     private javax.swing.JMenuItem opDeleteCa;
     private javax.swing.JMenuItem opDeleteMe;
+    private javax.swing.JMenuItem opDeleteMesa;
     private javax.swing.JMenuItem opDeleteP;
     private javax.swing.JMenuItem opListCa;
     private javax.swing.JMenuItem opListMe;
+    private javax.swing.JMenuItem opListMesa;
     private javax.swing.JMenuItem opListP;
     private javax.swing.JMenuItem opReadCa;
     private javax.swing.JMenuItem opReadMe;
+    private javax.swing.JMenuItem opReadMesa;
     private javax.swing.JMenuItem opReadP;
     private javax.swing.JMenuItem opUpdateCa;
     private javax.swing.JMenuItem opUpdateMe;
+    private javax.swing.JMenuItem opUpdateMesa;
     private javax.swing.JMenuItem opUpdateP;
     // End of variables declaration//GEN-END:variables
 }
